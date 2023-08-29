7141b523aa39872f2e754a1e2ac95d4c3bb55156
leandrofiore3 <leandrofiore3@gmail.com>
2023-08-29 14:29:59 -0300

    tests terminados

--- Server/src/test/index.test.js
+++ Server/src/test/index.test.js
@@ -2,6 +2,18 @@ const app = require('../app');
 const session = require('supertest');
 const request = session(app);
 
+const character = {
+  id: 923,
+  name: 'Leandro',
+  species: 'Human',
+  gender: 'Male',
+  status: 'Alive',
+  origin: {
+    name: 'Earth (C-137)',
+  },
+  image: 'image.jpg'
+};
+
 describe('Test de RUTAS', () => {
 
   describe('GET /rickandmorty/character/:id', () => {
@@ -13,21 +25,14 @@ describe('Test de RUTAS', () => {
     // SEGUNDO TEST
     it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
       const response = await request.get('/rickandmorty/character/1');
-      expect(response.status).toBe(200); // Asegura que la respuesta sea exitosa
-      const character = response.body; // Obtén el objeto del cuerpo de la respuesta
-      expect(character).toHaveProperty('id');
-      expect(character).toHaveProperty('name');
-      expect(character).toHaveProperty('species');
-      expect(character).toHaveProperty('gender');
-      expect(character).toHaveProperty('status');
-      expect(character).toHaveProperty('origin');
-      expect(character).toHaveProperty('image');
+      for(const prop in character) {
+        expect(response.body).toHaveProperty(prop);
+      }
     });
 
   it('Si hay un error responde con status: 500', async () => {
-    await request.get('/rickandmorty/character/51218').expect(500);// Si el ID es 0, responde con status 500
-    await request.get('/rickandmorty/character/827j').expect(500);// Si el ID es 827, responde con status 500
-    await request.get('/rickandmorty/character/abc').expect(500);// Si el ID es "abc", responde con status 500
+    const response = await request.get('/rickandmorty/character/51218j');
+    expect(response.status).toBe(500);
   });
   });
   
@@ -52,65 +57,31 @@ describe('Test de RUTAS', () => {
   // Describe para POST /rickandmorty/fav
   describe('POST /rickandmorty/fav', () => {
     // Primer test
-    it('Debería devolver un arreglo que contiene el elemento enviado por body', async () => {
-      const response = await request
-        .post('/rickandmorty/fav')
-        .send({ characterId: 1 });
-
-      expect(response.status).toBe(200);
-      const result = response.body;
-
-      expect(Array.isArray(result)).toBe(true);
-      expect(result).toContainEqual({ characterId: 1 });
+    it('Debe guardar el personaje en favoritos', async () => {
+      const response = await request.post('/rickandmorty/fav')
+      .send(character);
+      expect(response.body).toContainEqual(character);
     });
-
     // Segundo test
-    it('Debería devolver un arreglo que contiene todos los elementos enviados por body', async () => {
-      // Primero agregamos un elemento
-      await request.post('/rickandmorty/fav').send({ characterId: 1 });
-
-      // Luego agregamos otro elemento
-      const response = await request.post('/rickandmorty/fav').send({ characterId: 2 });
-
-      expect(response.status).toBe(200);
-      const result = response.body;
-
-      expect(Array.isArray(result)).toBe(true);
-      expect(result).toContainEqual({ characterId: 1 });
-      expect(result).toContainEqual({ characterId: 2 });
+    it('Debe agregar personajes a favoritos sin eliminar los existentes', async () => {
+      character.id = 1923;
+      character.name = 'Pedro pablo';
+      const response = await request.post('/rickandmorty/fav')
+      .send(character);
+      expect(response.body.lenght).toBe(2);
     });
   });
 
-   describe('DELETE /rickandmorty/fav/:id', () => {
-      // Primer test
-      it('Si el ID no existe, debe devolver el arreglo sin modificar', async () => {
-        // Agregamos un elemento
-        await request.post('/rickandmorty/fav').send({ characterId: 1 });
+  describe("DELETE /rickandmorty/fav/:id", () => {
+    it("Si el ID solicitado no existe, devuelve un arreglo con todos los favoritos", async () => {
+      const response = await request.delete("/rickandmorty/fav/djsakg45");
+      expect(response.body.lenght).toBe(2);
+    })
+    it("Si el ID solicitado existe, deberia eliminarlo de favoritos", async () => {
+      const response = await request.delete("/rickandmorty/fav/1923");
+      expect(response.body.lenght).toBe(1);
+    })
+  })
 
-        // Intentamos eliminar un elemento con un ID que no existe (por ejemplo, 999)
-        const response = await request.delete('/rickandmorty/fav/999');
-
-        expect(response.status).toBe(200);
-        const result = response.body;
-
-        expect(Array.isArray(result)).toBe(true);
-        expect(result).toContainEqual({ characterId: 1 }); // El elemento debe seguir en el arreglo
-      });
-
-      // Segundo test
-      it('Si el ID existe, debe eliminar correctamente el personaje', async () => {
-        // Agregamos un elemento
-        await request.post('/rickandmorty/fav').send({ characterId: 1 });
-
-        // Eliminamos el elemento con ID 1
-        const response = await request.delete('/rickandmorty/fav/1');
-
-        expect(response.status).toBe(200);
-        const result = response.body;
-
-        expect(Array.isArray(result)).toBe(true);
-        expect(result).not.toContainEqual({ characterId: 1 }); // El elemento ya no debe estar en el arreglo
-      });
-    });
 });
 
