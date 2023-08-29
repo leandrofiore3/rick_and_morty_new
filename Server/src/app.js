d2875465ee7d126897bb31b5cfb97e008bc9f0e3
leandrofiore3 <leandrofiore3@gmail.com>
2023-08-29 09:38:25 -0300

    modulizar server

---
+++ Server/src/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = require('./routes/index.js');
+const server = express();
+
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+     'Access-Control-Allow-Headers',
+     'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+     'Access-Control-Allow-Methods',
+     'GET, POST, OPTIONS, PUT, DELETE'
+  );
+  next();
+});
+
+
+server.use(express.json());
+server.use('/rickandmorty', router);
+
+module.exports = server;
+
+
+
+
+//**********************PRIMER FORMA QUE HICE SERVIDOR********************** */
+
+// const http = require('http');
+// const url = require('url');
+// const getCharById = require('./controllers/getCharById');
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   const parsedUrl = url.parse(req.url, true);
+//   const pathName = parsedUrl.pathname;
+
+//   if (pathName.includes('/rickandmorty/character')) {
+//     const id = pathName.split('/').at(-1);
+//     const characterId = parseInt(id);
+
+//     getCharById(res, characterId); // Llama al controlador getCharById con los parámetros requeridos
+//   } else {
+//     res.writeHead(404, { 'Content-Type': 'text/plain' });
+//     res.end('Not found');
+//   }
+// });
+
+// server.listen(3001, "localhost");
